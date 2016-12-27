ffc1ee8db3a388753b2c0a8b17e55bdc744b5b9d
罄天 <qinliang.ql@alibaba-inc.com>
2016-12-27 11:33:42 +0800

    添加注释

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var React = require('react'),
     withSideEffect = require('react-side-effect');
 
+//传入每一个被挂载的组件的props属性，然后让开发者自己控制应该返回的state属性
 function reducePropsToState(propsList) {
   var innermostProps = propsList[propsList.length - 1];
   if (innermostProps) {
@@ -10,6 +11,7 @@ function reducePropsToState(propsList) {
   }
 }
 
+//重新计算得到的state会被传入到这个函数
 function handleStateChangeOnClient(title) {
   var nextTitle = title || '';
   if (nextTitle !== document.title) {
@@ -17,13 +19,13 @@ function handleStateChangeOnClient(title) {
   }
 }
 
+//接受一个DocumentTitle组件，返回一个SideEffect实例
 var DocumentTitle = React.createClass({
   displayName: 'DocumentTitle',
-
   propTypes: {
     title: React.PropTypes.string.isRequired
   },
-
+  //每次实例化一个DocumentTitle都会调用该render方法
   render: function render() {
     if (this.props.children) {
       return React.Children.only(this.props.children);
@@ -33,6 +35,12 @@ var DocumentTitle = React.createClass({
   }
 });
 
+//调用方式如下：
+/*
+ <DocumentTitle title='Home'>
+    <h1>Home, sweet home.</h1>
+  </DocumentTitle>
+*/ 
 module.exports = withSideEffect(
   reducePropsToState,
   handleStateChangeOnClient
